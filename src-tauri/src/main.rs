7d545f2a79eae36f44ef3cfe336aa4ba1c370b94
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-04-21 10:46:42 -0300

    Add -f flag (format)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::path::Path;
+
 use serde::Deserialize;
 use youtube_dl::YoutubeDl;
 
@@ -15,28 +17,42 @@ struct DownloadRequest {
 
 // yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 "https://www.youtube.com/watch?v=oHg5SJYRHA0"
 // youtube-dl https://www.youtube.com/playlist?list=PLOU2XLYxmsILe6_eGvDN3GyiodoV3qNSC -A
+// yt-dlp -f "bv*+ba/b"
+// "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b"
 
 #[tauri::command(rename_all="snake_case")]
 fn download_video(download_request: DownloadRequest) -> String
 {
     println!("...Starting Download");
 
+    // let executable_path = Path::new("/usr/bin/yt-dlp");
+
     let output_template = download_request.artist + " - " + &download_request.track_name;
 
-    let output = YoutubeDl::new(&download_request.url)
-                     .download(true)
-                     .output_directory(download_request.output_dir)
-                     .output_template(output_template)
-                     .extract_audio(download_request.audio_only)
-                     .socket_timeout("15")
-                     .run();
+    let format = if download_request.audio_only {
+        // Audio only
+        "bestaudio[ext=m4a]/best[ext=mp3]"
+    } else {
+        "bestvideo*[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]"
+    };
+
+    let output =
+        YoutubeDl::new(&download_request.url)
+            // .youtube_dl_path(executable_path)
+            .download(true)
+            .output_directory(download_request.output_dir)
+            .output_template(output_template)
+            .format(format)
+            .extract_audio(download_request.audio_only)
+            .socket_timeout("15")
+            .run();
 
     println!("Download complete for url: {}", download_request.url);
 
     match output {
         Ok(_) => String::from("Download Complete"),
         Err(err) => {
-            println!("Error: {}", err);
+            eprintln!("Error: {}", err);
             String::from("Error occured. Could not download")
         }
     }
