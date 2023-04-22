07fc74adf33260b8d13b6281d81deeda9e4c6448
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-04-22 10:03:19 -0300

    Not more wrapper for yt-dlp using Command directly

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::path::Path;
+use std::process::Command;
 
 use serde::Deserialize;
-use youtube_dl::YoutubeDl;
 
 #[derive(Deserialize)]
 struct DownloadRequest {
@@ -17,40 +16,45 @@ struct DownloadRequest {
 
 // yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 "https://www.youtube.com/watch?v=oHg5SJYRHA0"
 // youtube-dl https://www.youtube.com/playlist?list=PLOU2XLYxmsILe6_eGvDN3GyiodoV3qNSC -A
+
+//# Formats for further reference (kinda hard to read)
 // yt-dlp -f "bv*+ba/b"
 // "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b"
+// "bestaudio[ext=m4a]/best[ext=mp3]"
+// "bestvideo*[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]"
 
 #[tauri::command(rename_all="snake_case")]
 fn download_video(download_request: DownloadRequest) -> String
 {
     println!("...Starting Download");
 
-    // let executable_path = Path::new("/usr/bin/yt-dlp");
+    let output_directory = format!(" -P \"{}\" ", download_request.output_dir);
+    let output_template = format!(" -o \"{} - {}\" ", download_request.artist,
+                                                      download_request.track_name);
+    let quoted_url = format!(" \"{}\" ", &download_request.url);
 
-    let output_template = download_request.artist + " - " + &download_request.track_name;
+    let mut yt_dlp_cmd = String::new();
+    yt_dlp_cmd.push_str("yt-dlp");
+    yt_dlp_cmd.push_str(&output_directory);
+    if download_request.audio_only {
+        // Requires (ffmpeg or ffprobe)
+        yt_dlp_cmd.push_str("--extract-audio");
+    }
+    yt_dlp_cmd.push_str(&output_template);
+    yt_dlp_cmd.push_str(&quoted_url);
+    yt_dlp_cmd.to_string();
 
-    let format = if download_request.audio_only {
-        // Audio only
-        "bestaudio[ext=m4a]/best[ext=mp3]"
-    } else {
-        "bestvideo*[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]"
-    };
-
-    let output =
-        YoutubeDl::new(&download_request.url)
-            // .youtube_dl_path(executable_path)
-            .download(true)
-            .output_directory(download_request.output_dir)
-            .output_template(output_template)
-            .format(format)
-            .extract_audio(download_request.audio_only)
-            .socket_timeout("15")
-            .run();
-
-    println!("Download complete for url: {}", download_request.url);
+    let output = Command::new("bash")
+                     .arg("-c")
+                     .arg(yt_dlp_cmd)
+                     .output();
 
     match output {
-        Ok(_) => String::from("Download Complete"),
+        Ok(_out) => {
+            println!("Download complete for url: {}", download_request.url);
+            // println!("LOG: {:?}", out);
+            String::from("Download Complete")
+        }
         Err(err) => {
             eprintln!("Error: {}", err);
             String::from("Error occured. Could not download")
