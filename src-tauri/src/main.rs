837069d823e059065e58ca5f5347322487ec5957
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-04-23 10:47:22 -0300

    Saving to intermadiate file in /tmp directory and removing it after convertion

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::process::Command;
-
+use std::{process::Command, fs};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -27,7 +26,9 @@ struct DownloadRequest {
 #[tauri::command(rename_all="snake_case")]
 fn download_video(download_request: DownloadRequest) -> String
 {
-    println!("...Starting Download");
+    println!("[START] Download and convertion");
+
+    const TEMP_FILE_PATH: &str = "/tmp/YT_DOWNLOADER_TEMP_FILE.webm";
 
     let format_string = if download_request.audio_only {
         // Audio only
@@ -43,8 +44,9 @@ fn download_video(download_request: DownloadRequest) -> String
         String::from(download_request.output_dir + "/")
     };
 
+    // TODO: use the '/tmp' folder to sabe this file and later remove it
     let output_directory = format!(" -P \"{}\" ", &output_dir);
-    let output_template = format!(" -o \"YT_DOWNLOADER_TEMP_FILE.webm\" ");
+    let output_template = format!(" -o \"{}\" ", TEMP_FILE_PATH);
     let format_flag = format!(" --format \"{}\" ", format_string);
     let quoted_url = format!(" \"{}\" ", &download_request.url);
 
@@ -55,14 +57,17 @@ fn download_video(download_request: DownloadRequest) -> String
     yt_dlp_cmd.push_str(&output_template);
     yt_dlp_cmd.push_str(&quoted_url);
 
+    println!("...Starting Download");
+
     let yt_dlp_output = Command::new("bash").arg("-c").arg(yt_dlp_cmd).output();
 
     if let Err(err) = yt_dlp_output {
         eprintln!("Error on yt-dlp download: {}", err);
         return "Error occured. Could not download".to_string();
     }
+    println!("...Intermediate file downloaded");
 
-    let input_flag = format!(" -i \"{}YT_DOWNLOADER_TEMP_FILE.webm\" ", &output_dir);
+    let input_flag = format!(" -i \"{}\" ", TEMP_FILE_PATH);
 
     let output_file = if download_request.audio_only {
         format!(" \"{}{} - {}.mp3\" ", output_dir, download_request.artist, download_request.track_name)
@@ -84,9 +89,14 @@ fn download_video(download_request: DownloadRequest) -> String
         eprintln!("Error to convert the video/audio: {}", err);
         return String::from("Error occured. Could not convert the video/audio");
     }
+    println!("...Intermediate file converted and named");
 
-    println!("ffmpeg stdout: {:?}", ffmpeg_output.unwrap().stdout);
+    match fs::remove_file(TEMP_FILE_PATH) {
+        Ok(_) => println!("...Intermediate file was removed"),
+        Err(err) => eprintln!("Error to remove intermediate file: {}", err)
+    }
 
+    println!("[END] Download and Convertion Complete");
     return String::from("Download and Convertion Complete");
 }
 
